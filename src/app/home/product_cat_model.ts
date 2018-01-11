a0d3ba04f64b5df8279aad83558526380e8853f8
Darshil Patel <darshil_21@yahoo.in>
2018-01-11 16:08:58 +0530

    aftr db chngd

--- src/app/home/product_cat_model.ts
+++ src/app/home/product_cat_model.ts
@@ -1,14 +1,14 @@
 export class product_cat {
-    public constructor(public pro_id: number,
-        public name: string,
-        public price: number,
-        public color: string,
-        public img1: string,
-        public img2: string,
-        public img3: string,
-        public desci: string,
+    public constructor(public pk_pro_id: number,
+        public pro_name: string,
+        public pro_price: number,
+        public pro_color: string,
+        public pro_img1: string,
+        public pro_img2: string,
+        public pro_img3: string,
+        public pro_desci: string,
         public fk_cat_id: number,
-        public cat_id: number,
+        public pk_cat_id: number,
         public cat_name: string
     ) { }
 }
